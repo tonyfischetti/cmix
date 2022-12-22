94d38ce18109c211e669ada5a13034cfb7f184fb
Tony Fischetti <tony.fischetti@gmail.com>
2022-12-22 10:29:28 -0500

    added script to xfer playlists for navidrome

---
+++ make-for-navidrome.lisp
@@ -0,0 +1,16 @@
+#!/usr/local/bin/lispscript
+
+(defvar /all-playlists/ (ls "./playlists/"))
+
+(defvar /dest-base/ "/home/tony/music/_PLAYLISTS/")
+
+
+(for-each /all-playlists/
+  (progress-bar index! (length /all-playlists/))
+  (let* ((tmp (-path value! #P"/home/tony/cmus/playlists/"))
+         (dest (fn "~A~A.m3u" /dest-base/ tmp)))
+    (with-a-file dest :w
+      (for-each value!
+        (let ((xlation (substr value! 6)))
+          (format stream! "~A~%" xlation))))))
+
